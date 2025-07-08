67d72126c6976bb001d52c54612ed14c469c9097
nbookman <nbookman@gmail.com>
2025-07-08 15:52:47 -0700

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,47 +4,47 @@ const tableauExt = window.tableau.extensions;
 //Wrap everything into an anonymous function
 (function () {
     async function init() {
-        //clean up any divs from the last initialization
+        // Clean up any divs from the last initialization
         $('body').empty();
         tableau.extensions.setClickThroughAsync(true).then(() => {
             let dashboard = tableauExt.dashboardContent.dashboard;
-            //Loop through the Objects on the Dashboard and render the HTML Objects
+            // Loop through the Objects on the Dashboard and render the HTML Objects
             dashboard.objects.forEach(obj => {
                 render(obj);
-            })
+            });
         }).catch((error) => {
             // Can throw an error if called from a dialog or on Tableau Desktop
             console.error(error.message);
         });
     }
 
-    function getMarginFromObjClasses(objClasses){
+    function getMarginFromObjClasses(objClasses) {
         const margin = [0, 0, 0, 0];
         if (!objClasses) return margin;
 
-        const classNames = objClasses.split(/\s+/)
+        const classNames = objClasses.split(/\s+/);
         classNames.reverse();
         const marginClass = classNames.find((cl) => cl.startsWith('margin-'));
         if (!marginClass) return margin;
 
-        const marginValues = marginClass.split('-').slice(1).map(v => parseInt(v))
+        const marginValues = marginClass.split('-').slice(1).map(v => parseInt(v));
         if (marginValues.length === 1) {
-            const [all] = marginValues
-            return [all, all, all, all]
+            const [all] = marginValues;
+            return [all, all, all, all];
         }
 
         if (marginValues.length === 2) {
-            const [vertical, horizontal] = marginValues
-            return [vertical, horizontal, vertical, horizontal]
+            const [vertical, horizontal] = marginValues;
+            return [vertical, horizontal, vertical, horizontal];
         }
 
         if (marginValues.length === 3) {
-            const [top, horizontal, bottom] = marginValues
-            return [top, horizontal, bottom, horizontal]
+            const [top, horizontal, bottom] = marginValues;
+            return [top, horizontal, bottom, horizontal];
         }
 
         if (marginValues.length === 4) {
-            return marginValues
+            return marginValues;
         }
 
         return margin;
@@ -52,31 +52,31 @@ const tableauExt = window.tableau.extensions;
 
     async function render(obj) {
         let objNameAndClasses = obj.name.split("|");
-        //Parse the Name and Classes from the Object Name
+        // Parse the Name and Classes from the Object Name
         let objId = objNameAndClasses[0];
         let objClasses;
-        //Check if there are classes on the object
+        // Check if there are classes on the object
         if (objNameAndClasses.length > 1) {
             objClasses = objNameAndClasses[1];
         }
-        //Create the initial object with CSS Props
-        
+        // Create the initial object with CSS Props
+
         // we need to check for padding classes first, as they must be handled via positioning
-        const margin = getMarginFromObjClasses(objClasses)
-        
-        //Here we set the CSS props to match the location of the objects on the Dashboard
+        const margin = getMarginFromObjClasses(objClasses);
+
+        // Here we set the CSS props to match the location of the objects on the Dashboard
         let props = {
             id: `${objId}`,
             css: {
                 'position': 'absolute',
-                top: `${obj.position.y + margin[0] - window.scrollY}px`,
-                left: `${obj.position.x + margin[3] - window.scrollX}px`,
+                top: `${obj.position.y + margin[0]}px`,       // <-- Removed subtraction of window.scrollY
+                left: `${obj.position.x + margin[3]}px`,      // <-- Removed subtraction of window.scrollX
                 'width': `${parseInt(obj.size.width) - margin[1] - margin[3]}px`,
                 'height': `${parseInt(obj.size.height) - margin[0] - margin[2]}px`
             }
-        }
+        };
         let $div = $('<div>', props);
-        //Add the class to the HTML Body
+        // Add the class to the HTML Body
         $div.addClass(objClasses);
         $('body').append($div);
     }
@@ -84,12 +84,11 @@ const tableauExt = window.tableau.extensions;
     $(document).ready(() => {
         tableauExt.initializeAsync().then(() => {
             init();
-            //Register an event handler for Dashboard Object resize
-            //Supports automatic sized dashboards and reloads
-            let resizeEventHandler = tableauExt.dashboardContent.dashboard.addEventListener(tableau.TableauEventType.DashboardLayoutChanged, init);
+            // Register an event handler for Dashboard Object resize
+            // Supports automatic sized dashboards and reloads
+            tableauExt.dashboardContent.dashboard.addEventListener(tableau.TableauEventType.DashboardLayoutChanged, init);
         }, (err) => {
-            console.log("Broken")
+            console.log("Broken");
         });
     });
-
 })();
